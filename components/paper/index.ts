ea07a41f78eb5240727c122255f9566af95482ab
Ayushs135 <ayushs01032005@gmail.com>
2026-09-16 16:35:28 +0530

    feat(ui): export examination paper UI components

---
+++ components/paper/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Examination Paper UI Presentation Components
+ *
+ * Export barrel for:
+ * - PaperView: Master authentic examination document container
+ * - PaperHeader: CBSE examination header & metadata bar
+ * - PaperSection: Section container (MCQ, Short Answer, Long Answer)
+ * - PaperQuestion: Individual question renderer with swap & AI enhancement triggers
+ * - PaperGenerationSummary: Real-time generation diagnostics and breakdown drawer
+ */
+
+export * from "./PaperHeader";
+export * from "./PaperQuestion";
+export * from "./PaperSection";
+export * from "./PaperGenerationSummary";
+export * from "./PaperView";
+
